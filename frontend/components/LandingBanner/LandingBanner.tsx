b078d7c772a7b7cf2c4115f6d749db74c0ac7fbc
Milan White <milan.white.25@gmail.com>
2025-06-10 23:42:04 -0400

    refactor: more alterations to gradient on portable view

--- frontend/components/LandingBanner/LandingBanner.tsx
+++ frontend/components/LandingBanner/LandingBanner.tsx
@@ -9,7 +9,7 @@ const LandingBanner: React.FC = () => {
             className="relative lg:h-[70vh] bg-cover bg-center"
             style={{ backgroundImage: `url(${LandingBannerImage})` }}
         >
-            <div className="absolute inset-0 z-10 bg-gradient-to-r from-black lg:via-primary/30 to-primary/50 lg:to-transparent" />
+            <div className="absolute inset-0 z-10 bg-gradient-to-r from-black via-primary-dark/75 lg:via-primary/30 to-primary/50 lg:to-transparent" />
 
             <div className="relative z-10 text-white">
                 <LandingCTA />
